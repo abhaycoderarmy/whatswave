0c3a29db1cb68f7b64cf7c736fa6461323a36606
abhaycoderarmy <2320403202@stu.manit.ac.in>
2024-07-26 12:12:39 +0530

    first commit

---
+++ whatswave/server/models/UserModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type : String,
+        required : [true,"provide your name"]
+    },
+    email : {
+        type : String,
+        required : [true, "provide your email id"],
+        unique : true
+
+    },
+    password : {
+        type : String,
+        required : [true,"provide your password"]
+    },
+    profile_pic : {
+        type : String,
+        default : ""
+    }
+},{
+    timestamps : true
+})
+
+const UserModel = mongoose.model('User',userSchema)
+
+module.exports =UserModel
